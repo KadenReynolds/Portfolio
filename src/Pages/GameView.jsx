b6bc443e2130e2804d1c7deb6cdf88d5f7914dd7
KadenR <kadenreynolds05@gmail.com>
2024-08-23 14:15:56 -0600

    finished css on gameview page next will be making the download button function

--- src/Pages/GameView.jsx
+++ src/Pages/GameView.jsx
@@ -1,20 +1,41 @@
 import { useParams } from "react-router-dom"
 import { useState } from "react";
+import EssenceScreenShot from '../assets/EssenceScreenShot.png'
 
 
 function GameView ({myGames}) {
   const {gameID} = useParams();
   // const [gameInfo, setGameInfo] = useState([])
   let gameInfo = myGames[gameID]
+  let gameImg
+  if(gameInfo.id === 0){
+    gameImg = EssenceScreenShot
+  }
 
   return(
-    <div>
+    <div className="gameViewDiv">
       <br />
       <h1>{gameInfo.name}</h1>
-      <label htmlFor="timeCreated">Development Time:</label>
-      <h5 id="timeCreated">{gameInfo.timeCreated}</h5>
-      <h5>{gameInfo.status}</h5>
-      <img src={gameInfo.gameScreenShot} alt="hello" />
+      <img className="gameImg" src={gameImg} alt="" />
+      <br />
+      <h3 className="gameInfoHeader">Game Info:</h3>
+      <table className="gameInfoTable">
+        <tr>
+        </tr>
+        <tr>
+          <th>Development Time</th>
+          <br />
+          <th>Status</th>
+        </tr>
+        <tr>
+          <td>{gameInfo.timeCreated}</td>
+          <br />
+          <td>{gameInfo.status}</td>
+        </tr>
+      </table>
+      <br />
+      <button className="downloadButton">Download</button>
+      <p className="buttonNote">*Note: This button will redirect you to my page on Itch.io to download these games</p>
     </div>
   )
 }
